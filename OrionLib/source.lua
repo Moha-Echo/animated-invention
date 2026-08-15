390720829c32cd6bb628e256312433dbcbec0ef5
Moha-Echo <moha.msi2023@gmail.com>
2026-08-15 05:08:58 +0200

    Rename ScreenGui from 'Orion' to 'OrangeHelper'

--- OrionLib/source.lua
+++ OrionLib/source.lua
@@ -245,7 +245,7 @@ local function GetIcon(IconName)
 end   
 
 local Orion = Instance.new("ScreenGui")
-Orion.Name = "Orion"
+Orion.Name = "OrangeHelper"
 if syn then
 	syn.protect_gui(Orion)
 	Orion.Parent = game.CoreGui
